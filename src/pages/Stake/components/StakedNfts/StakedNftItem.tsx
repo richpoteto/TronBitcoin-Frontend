547532ba12218f4c1f086fe8bc9068666bb5a94c
richpoteto <richjiang.soft@gmail.com>
2023-04-18 04:49:02 -0700

    Modified MUI button

--- src/pages/Stake/components/StakedNfts/StakedNftItem.tsx
+++ src/pages/Stake/components/StakedNfts/StakedNftItem.tsx
@@ -62,74 +62,85 @@ const StakedNftItem = ({ address, id, mp, claimable, stakedDate, newtrons }: Sta
         setDisable(false);
       }, 4 * 60 * 1000);
     }
-  } 
-  , [disable])
+  }
+    , [disable])
 
-return (
-  <>
-    <TableRow
-      sx={{
-        backgroundColor: "common.black",
-        "&:not(:first-of-type)": {
-          borderTop: "3px solid #22274f",
-        },
-      }}
-    >
-      <TableCell sx={{ borderBottom: "none", width: "140px" }}>
-        <Box
-          component="img"
-          src={NftImage}
-          sx={{ width: "100%", aspectRatio: "1", borderRadius: "8px" }}
-        />
-      </TableCell>
-      <TableCell sx={{ borderBottom: "none" }}>
-        <Typography color="white" fontSize="14px" mb="4px">
-          Staking since
-        </Typography>
-        <Typography
-          color="white"
-          fontSize="18px"
-          fontFamily="Audiowide"
-          mb="4px"
-        >
-          {day}
-        </Typography>
-        <Typography color="white" fontSize="14px" fontFamily="Audiowide">
-          {hour}
-        </Typography>
-      </TableCell>
-      <TableCell sx={{ borderBottom: "none" }}>
-        <Typography color="white" fontSize="14px" mb="4px">
-          Mined Tokens
-        </Typography>
-        <Typography
-          color="success.main"
-          fontSize="16px"
-          fontFamily="Audiowide"
-          mb="8px"
-        >
-          {newtrons}
-        </Typography>
-        <Box>
-          <Button variant="contained" sx={{ mr: "8px" }} onClick={_unStakeNft}>
-            Unstake
-          </Button>
-          <Button variant="contained" onClick={_claimNft} disabled={!claimable || disable}>Claim</Button>
-        </Box>
-      </TableCell>
-    </TableRow>
-    <TableRow sx={{ backgroundColor: "common.black" }}>
-      <TableCell colSpan={4} sx={{ p: "4px 16px", borderBottom: "none" }}>
-        <Typography component="span" color="white" fontSize="14px" mr="16px">
-          MP: {mp}
-        </Typography>
-        <Typography component="span" color="white" fontSize="14px">
-          RANK #{id}
-        </Typography>
-      </TableCell>
-    </TableRow>
-  </>
-);
+  return (
+    <>
+      <TableRow
+        sx={{
+          backgroundColor: "common.black",
+          "&:not(:first-of-type)": {
+            borderTop: "3px solid #22274f",
+          },
+        }}
+      >
+        <TableCell sx={{ borderBottom: "none", width: "140px" }}>
+          <Box
+            component="img"
+            src={NftImage}
+            sx={{ width: "100%", aspectRatio: "1", borderRadius: "8px" }}
+          />
+        </TableCell>
+        <TableCell sx={{ borderBottom: "none" }}>
+          <Typography color="white" fontSize="14px" mb="4px">
+            Staking since
+          </Typography>
+          <Typography
+            color="white"
+            fontSize="18px"
+            fontFamily="Audiowide"
+            mb="4px"
+          >
+            {day}
+          </Typography>
+          <Typography color="white" fontSize="14px" fontFamily="Audiowide">
+            {hour}
+          </Typography>
+        </TableCell>
+        <TableCell sx={{ borderBottom: "none" }}>
+          <Typography color="white" fontSize="14px" mb="4px">
+            Mined Tokens
+          </Typography>
+          <Typography
+            color="success.main"
+            fontSize="16px"
+            fontFamily="Audiowide"
+            mb="8px"
+          >
+            {newtrons}
+          </Typography>
+          <Box>
+            <Button variant="contained" sx={{ mr: "8px" }} onClick={_unStakeNft}>
+              Unstake
+            </Button>
+            <Button
+              variant="contained"
+              onClick={_claimNft}
+              disabled={!claimable || disable}
+              sx={{
+                "&.Mui-disabled": {
+                  background: "gray",
+                  color: "#c0c0c0"
+                }
+              }}>
+              Claim
+            </Button>
+          </Box>
+        </TableCell>
+      </TableRow>
+      <TableRow sx={{ backgroundColor: "common.black" }}>
+        <TableCell colSpan={4} sx={{ p: "4px 16px", borderBottom: "none" }}>
+          <Typography component="span" color="white" fontSize="14px" mr="16px">
+            MP: {mp}
+          </Typography>
+          <Typography component="span" color="white" fontSize="14px">
+            RANK #{id}
+          </Typography>
+        </TableCell>
+      </TableRow>
+    </>
+  );
 };
 
 export default StakedNftItem;
